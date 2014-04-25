1660d7924aefa0bd5b4a1bcaddbd7fc2591261ac
fedeagb@gmail.com <fedeagb@gmail.com@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-04-25 20:05:27 +0000

    Modificacion nuevamente del log. Comienzo de algunas cosas del initializer

--- Tools/initializer.sh
+++ Tools/initializer.sh
@@ -2,11 +2,59 @@
 # Initializer que por ahora solo inicia variables de ambiente, con la estructura de directorios actuales
 # Para que las inicie, hay que correr este script con ". initializer.sh" para que este en el contexto de la consola, sino no las setea (y no se puede hacer desde script, ya probe y busque...)
 
-numfields=`echo \`pwd\` | grep -o '/' | wc -l`
-numfields=`expr $numfields + 1` # le sumo 1 pues cut toma como field antes del / incial tambien
-pathconf=`echo \`pwd\` | cut -f"$numfields" -d'/' --complement` # llego hasta ../grupo03
-pathconf="$pathconf/conf"
+### Variables a setear
 
-export LOGDIR=`echo \`grep '^LOGDIR' "$pathconf"/installer.conf\` | cut -f2 -d'='` # Suponiendo que LOGDIR tenga el path completo, sino se lo tengo que agregar
-export LOGEXT=`echo \`grep '^LOGEXT' "$pathconf"/installer.conf\` | cut -f2 -d'='` # Fijarse tambien que usamos como separador en las lineas!!
-export LOGSIZE=`echo \`grep '^LOGSIZE' "$pathconf"/installer.conf\` | cut -f2 -d'='` # En B
+GRUPO=""
+CONFDIR=""
+BINDIR=""
+MAEDIR=""
+NOVEDIR=""
+DATASIZE=""
+ACEPDIR=""
+RECHDIR=""
+INFODIR=""
+LOGDIR=""
+LOGEXT=""
+LOGSIZE=""
+
+# Variables que leemos del archivo de configuracion
+VARARCHCONF=("GRUPO" "CONFDIR" "BINDIR" "MAEDIR" "NOVEDIR" "DATASIZE" "ACEPDIR" "RECHDIR" "INFODIR" "LOGDIR" "LOGEXT" "LOGSIZE")
+
+# Variables que tienen información de path a directorios
+VARPATH=("GRUPO" "CONFDIR" "BINDIR" "MAEDIR" "NOVEDIR" "ACEPDIR" "RECHDIR" "INFODIR" "LOGDIR")
+
+# Variales que tienen información de data (no path)
+VARLOG=("LOGEXT" "LOGSIZE")
+
+# Variable Datasize esta aparte
+
+### Paths base
+
+ACTUAL="`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd`"
+PATHGROUP=`echo "$ACTUAL" | sed "s-\(.*\)/grupo03/.*-\1/grupo03-"`
+PATHCONF="$PATHGROUP/conf"
+
+function cargarVariablesConf
+{
+	for i in "${VARARCHCONF[@]}"
+	do
+		eval "$i"=`echo \`grep '^"$i"' "$PATHCONF"/installer.conf\` | cut -f2 -d'='`
+	done
+
+	return 0
+}
+
+
+
+### Comienzo script
+
+if [ $# -eq "1" ]
+then
+	if [ $1 == "-f" ]
+	then
+		unset ENVINIT
+		exit 0
+	fi
+fi
+
+

9a3849ec482d2afa06f422219a4f80c6b229ff8f
fedeagb@gmail.com <fedeagb@gmail.com@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-04-25 20:05:27 +0000

    Modificacion nuevamente del log. Comienzo de algunas cosas del initializer

--- trunk/Tools/logging.sh
+++ trunk/Tools/logging.sh
@@ -5,10 +5,20 @@
 # INFO = INFORMATIVO: mensajes explicativos sobre el curso de ejecución del comando.
 # WAR = WARNING: mensajes de advertencia pero que no afectan la continuidad de ejecución del comando.
 # ERR = ERROR: mensajes de error.
+# Por default, si no se le pase 3er parametro, el tipo de mensaje es informativo
 
-if [ $# -lt 3 ]
+function reducir
+{
+	tail -50 "$1" > "$1.tmp" # Me quedo con las ultimas 50 lineas
+	mv "$1.tmp" "$1" # Piso el viejo con el nuevo
+	echo "`date +"%F %T"`-Log excedido para poder controlar que se esté realizando este trabajo" >> "$1"
+
+	return 0
+}
+
+if [ $# -lt 2 ] # El 3ro se puede no pasar nada y queda el default
 then
-	echo "Faltan parametros" # Esto deberia ir a un log? Idem para el resto de las verificaciones
+	echo "Faltan parametros" 
 	exit -1
 fi 
 if [ $# -gt 3 ]
@@ -16,41 +26,46 @@ then
 	echo "Hay parametros demas"
 	exit -1
 fi
-if [ $3 != 'INF' -a $3 != 'WAR' -a $3 != 'ERR' ]
+ 
+if [ $1 == "installer" ]
 then
-	echo "El tercer parametro no es ni INF ni WAR ni ERR"
-	exit -1
+	if [ -z ${CONFDIR} ]
+	then
+		echo "No esta seteada la variable CONFDIR"
+		exit -3
+	elif [ -z ${GRUPO} ]
+	then
+		echo "No esta seteada la variable GRUPO"
+		exit -3
+	fi
+	dir="$GRUPO/$CONFDIR" # El instalador debe setear variables GRUPO y CONFDIR para poder usar el logging
+	LOGEXT="log"
+else
+	dir="$GRUPO/$LOGDIR"
 fi
 
 when=`date +"%F %T"`
 who=$USER
 where=$1
 why=$2
-what=$3
+if [ $# == 2 ]
+then
+	what='INF'
+else
+	what=$3
+fi
 
-LOGNAME="$where" # Si se pasa el comando con .sh habria que sacar el .sh de acá
+LOGNAME="$where"
 
-echo "$when-$who-$where-$what-$why" >> "$LOGDIR/$LOGNAME.$LOGEXT"
 
-size=`wc -c < "$LOGDIR/$LOGNAME.$LOGEXT"`
+echo "$when-$who-$where-$what-$why" >> "$dir/$LOGNAME.$LOGEXT"
+
+size=`wc -c < "$dir/$LOGNAME.$LOGEXT"`
 size=`expr $size / 1024` # lo paso a KB
 
 if [ $size -gt $LOGSIZE ]
 then
-	reducir "$LOGDIR/$LOGNAME.$LOGEXT"
+	reducir "$dir/$LOGNAME.$LOGEXT"
 fi
 
 exit 0
-
-
-function reducir
-{
-	lineas=`wc -l < $1`
-	par=`expr $lineas - 50` # Para dejar las ultimas 50 lineas del log
-	split -l $par $1 $1 # el split divide a archivo con prefijoaa .. ab .. (prefijo = $3)
-	rm $1 # Borro el viejo
-	rm "$1""aa" 
-	mv "$1""ab" "$1" 
-
-	return 0
-}

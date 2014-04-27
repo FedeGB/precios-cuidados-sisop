ac0701f0fbca4c58a7b6d2eebe71aef74af958eb
fedeagb@gmail.com <fedeagb@gmail.com@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-04-27 20:25:53 +0000

    Casi completado el Mover, hace practicamente todo y esta probado (creo que no me olvide de probar ninguno de los casos, pero si encuentran algun error chiflen). Lo unico que me falta ver es el tema de que el comando mv tenga algun error fuera de los comunes que estan puestos y que la funcion devuelva != 0 en esos casos. Recuerden tambien que si se le pasa el parametro  va a usarla funcion logging, asi que si prueban con el Mover tengan las variables de ambiente que usa el logging seteadas

--- trunk/Tools/Mover.sh
+++ trunk/Tools/Mover.sh
@@ -2,7 +2,8 @@
 # Función mover para RETAILC
 # Pre: Recibe $1: origen, $2: destino, $3: comando invocador
 # $1 y $2 son parametros obligatorios. $3 es opcional, si el comando invocador utiliza
-# archivos de log se le debe pasar $3. Mover escribirá el resultado de su acción en el mismo
+# archivos de log se le debe pasar $3. Mover escribirá el resultado de su acción en el mismo log.
+# Post: termina con 0 en caso de éxito, otro numero en otro casos.
 
 if [ $# -lt 2 ]
 then
@@ -18,16 +19,60 @@ fi
 ORG=$1
 DEST=$2
 
+if [ $# -eq 3 ]
+then
+	CMD=$3
+fi
+
 if ! [ -f $ORG ]
 then
-	./logging.sh "Mover" "No existe el archivo de origen" "ERR" 
-	echo "No existe el archivo de origen"
+	if ! [ -z $CMD ]
+	then
+		./logging.sh "$CMD" "No existe el archivo de origen: $ORG" "ERR" 
+	fi
+	echo "No existe el archivo de origen: $ORG"
 	exit -2
 fi
+
 DESTDIR=$( echo "$DEST" | sed "s-\(.*\)/.*\$-\1-")
+DESTNAME=$( echo "$DEST" | sed "s-\(.*\)/\(.*\)\$-\2-")
 if ! [ -d $DESTDIR ]
 then
-	./logging.sh "Mover" "No existe el directorio de destino" "ERR" 
+	if ! [ -z $CMD ]
+	then
+		./logging.sh "$CMD" "No existe el directorio de destino: $DESTDIR" "ERR" 
+	fi
 	echo "No existe el directorio de destino"
 	exit -2
 fi
+
+if [ -f $DEST ]
+then
+	if ! [ -d "$DESTDIR/dup" ]
+	then
+		mkdir "$DESTDIR/dup"
+		mv "$ORG" "$DESTDIR/dup/$DESTNAME.1"
+		if ! [ -z $CMD ]
+		then
+			./logging.sh "$CMD" "Se movio $ORG a $DESTDIR/dup/$DESTNAME.1 pues $DEST ya existia"
+		fi
+		exit 0
+	else
+		ACTN=$(ls "$DESTDIR/dup" -1 | grep "$DESTNAME\.[0-9]\{1,3\}" -c)
+		N=$(expr $ACTN + 1)
+		mv "$ORG" "$DESTDIR/dup/$DESTNAME.$N"
+		if ! [ -z $CMD ]
+		then
+			./logging.sh "$CMD" "Se movio $ORG a $DESTDIR/dup/$DESTNAME.$N pues $DEST ya existia"
+		fi
+		exit 0
+	fi
+else
+	mv "$ORG" "$DEST"
+	if ! [ -z $CMD ]
+		then
+			./logging.sh "$CMD" "Se movio $ORG a $DEST"
+	fi
+fi
+
+exit 0

970e74ccf7a13cdd13a6c007d74d081c537ef6f6
mauriciodbazan <mauriciodbazan@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-04-29 07:16:01 +0000

    Added some test-directories, created sameUnit function

--- Rating/rating.sh
+++ Rating/rating.sh
@@ -3,6 +3,10 @@
 ERROR=0
 ACEPDIR=/home/ubuntu/precios-cuidados-sisop/Rating/aceptados
 PROCDIR=/home/ubuntu/precios-cuidados-sisop/Rating/procesados
+RECHDIR=/home/ubuntu/precios-cuidados-sisop/Rating/rechazados
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+MOVER='../Tools/Mover.sh'
+TABLA="../Datos/Maestros y tablas/um.tab"
 
 <<CheckFile 
 	Checks that a file is not empty nor already processed. Returns zero if OK.
@@ -20,4 +24,22 @@ function checkFile() {
 		echo "0"
 }
 
-checkFile lista2
+<<sameUnit
+	Checks if 2 units are actually the same, by using the unit table.	
+sameUnit
+
+function sameUnit() {
+	declare local line1
+	declare local line2
+	line1=$(cat "$TABLA" | grep $1 -n | sed 's/\([^;]*\):.*/\1/')
+	line2=$(cat "$TABLA" | grep $2 -n | sed 's/\([^;]*\):.*/\1/')	
+	if [[ "$line1" = "$line2" ]]; then
+		echo "0"
+		return
+	fi
+	echo "1"
+	return
+}
+
+resultado=$(sameUnit kg k)
+echo $resultado

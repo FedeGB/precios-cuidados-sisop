b1beab62265e344861770d8cbcbd7b5448d28778
fedeagb@gmail.com <fedeagb@gmail.com@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-04-30 02:37:02 +0000

    Arreglo por un tema de los log, la funcion start tambien recibe el nombre del comando que la llama, asi sabe en que log escribir. Por ende, si es una funcion que no graba en archivos de log, simplemente se pasa un NULL como primer parametro

--- trunk/Tools/Start.sh
+++ trunk/Tools/Start.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # Función Start para RETAILC
 # La función comienza procesos procesos o en foreground o en background
-# Pre: Recibe como mínimo 2 parámetros. Deben estar inicializadas las variables de ambiente (ENVINIT=1)
-# $1: Tipo de ejecución (-f: foreground, -b: background) ambos en el contexto de quien lo ejecuta
-# $2: Nombre del comando (sin .sh)
+# Pre: Recibe como mínimo 3 parámetros. Deben estar inicializadas las variables de ambiente (ENVINIT=1)
+# $1: Proceso caller (nombre del comando que esta utilizando el Start, sin el .sh). Si se le pasa NULL,
+# no graba un archivo de log, en caso de ser un comando que no usa archivos de log.
+# $2: Tipo de ejecución (-f: foreground, -b: background) ambos en el contexto de quien lo ejecuta
+# $3: Nombre del comando (sin .sh)
 # $N: Parametros para pasar al comando a ejecutar
 
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
 	echo "Faltan parámetros"
 	exit -1
 fi
 
-if ! [ $1 == "-f" -o $1 == "-b" ]
+if ! [ $2 == "-f" -o $2 == "-b" ]
 then
 	echo "El primer parametro es incorrecto"
 	exit -1
 fi
 
-CMD="$2.sh"
+CALL="$1"
+TIPO="$2"
+PRO="$3"
+CMD="$3.sh"
 
 PID=$(pgrep "$CMD")
 
@@ -30,17 +35,19 @@ fi
 
 if ! [ -z $PID ]
 then
-	./logging "$2" "El proceso $1 ya está corriendo" "WAR"
+	if ! [ $CALL == "NULL" ]
+	then
+	./logging "$CALL" "El proceso $PRO ya está corriendo" "WAR"
+	fi
+	echo "El proceso $PRO ya está corriendo"
 	exit 0
 fi
 
-
-
-if [ $# -eq 2 ]
+if [ $# -eq 3 ]
 then
 	if [ "$TIPO" == "-f" ]
 	then
-		./"$CMD"
+		./$CMD
 	else
 		./$CMD &
 	fi
@@ -49,12 +56,16 @@ else
 	declare -a PAR=("$@")
 	if [ "$TIPO" == "-b" ]
 	then
-		./$CMD "${PAR[@]:2:$last}" &
+		./"$CMD" "${PAR[@]:3:$last}" &
 	else
-		./"$CMD" "${PAR[@]:2:$last}"
+		./"$CMD" "${PAR[@]:3:$last}"
 	fi
 	
 fi
 
-./logging "$2" "Comenzo la ejecución de $CMD con PID: $PID"
+if ! [ $CALL == "NULL" ]
+then
+	./logging "$CALL" "Comenzo la ejecución de $PRO"
+fi
+
 exit 0

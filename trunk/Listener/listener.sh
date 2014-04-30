39205dd52fdd59905ba68f7ec17f66633d26048e
ivi.rhcp <ivi.rhcp@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-04-30 02:50:11 +0000

    Commit para Mauri que lo mira por tv (?)

--- trunk/Listener/listener.sh
+++ trunk/Listener/listener.sh
@@ -20,21 +20,55 @@ function aceptar
 
 }
 
-function esListaDeCompras
+#Valida que un usuario sea asociado
+#Parámetros:
+#$1 -> Nombre de usuario
+#$2 -> "Y" si se quiere chequear que sea colaborador, cualquier otra cosa no chequea
+function usuario_es_asociado
 {
-	declare local usuario=`echo $1 | grep "^\([^\.]\)*\.[^-\s]\{3\}" | sed 's~^\([^\.]*\)\.[^-\s]\{3\}~\1~'`
-	if [[ "$usuario" == "" ]]; then
-		#return False
-	else
-		declare local asociado=`cat "$MAEDIR"asociados.mae | grep "^[^;]*;[^;]*;$usuario;[0 | 1];[^@]*@[^@]*\.[^@]*$" |  
-		sed "s~^[^;]*;[^;]*;\($usuario\);[0 | 1];[^@]*@[^@]*\.[^@]*\$~\1~"`
-		if [[ "$asociado" == "$usuario" ]]; then
-			#return True
-		else
-			#return False
+	declare local validationData=`cat "$MAEDIR"asociados.mae | grep "^[^;]*;[^;]*;$1;[0 | 1];[^@]*@[^@]*\.[^@]*$" |  
+		sed "s~^[^;]*;[^;]*;\($1\);\([0 | 1]\);[^@]*@[^@]*\.[^@]*\$~\1-\2~"`
+	if [[ "$2" == "Y" ]]; then
+		declare local colaborador=`echo $validationData | sed "s~^$1-\([0 | 1]\)$~\1~"`
+		if [[ "$colaborador" == "" ] || [ "$colaborador" == 0 ]]; then
+			return 0
 		fi
 	fi
+	declare local asociado=`echo $validationData | sed "s~^\($1\)-[0 | 1]$~\1~"`
+	if [[ "$asociado" == "$1" ]]; then
+		return 1
+	else
+		return 0
+	fi
 }
+
+function es_lista_compras
+{
+	declare local usuario=`echo $1 | grep "^\([^\.]\)*\.[^- ]\{3\}$" | sed 's~^\([^\.]*\)\.[^- ]\{3\}$~\1~'`
+	if [[ "$usuario" == "" ]]; then
+		return 0
+	else
+		usuario_es_asociado $usuario
+		return $? 
+	fi
+}
+
+function es_lista_precios
+{
+	declare local validationData=`echo $1 | grep "^[^ ]*-[0-9]\{8\}\..*$" | sed 's~^[^ ]*-\([0-9]\{8\}\)\.\(.*\)$~\1-\2'`
+	declare local fecha=`echo $validationData | sed "s~^\([0-9]\{8\}\)-.*$~\1~"`
+	validar_fecha $fecha
+	if [[ $? == 0 ]]; then
+		return 0
+	fi
+	declare local colaborador=`echo $validationData | sed 's~^[0-9]\{8\}-\(.*\)$~\1~'`
+	usuario_es_asociado $colaborador "Y"
+	if [[ $? == 0 ]]; then
+		return 0
+	fi
+	return 1
+}
+
 
 for arch in `ls $NOVEDIR`;
 do
@@ -44,10 +78,10 @@ do
 	then
 		echo Rechazado #rechazar $arch
 	else
-		if [[ esListaDeCompras $arch ]]; then
+		if [[ es_lista_compras $arch ]]; then
 			#Aceptar
 		else #ver de cambiar a elif
-			if [[ condition ]]; then
+			if [[ es_lista_precios ]]; then
 				#statements
 			fi
 		fi

484a23d3e38d93af07d1f6253449b7b70a72bb5c
fedeagb@gmail.com <fedeagb@gmail.com@6c7d8a13-44de-03e9-802f-3708b9ba4372>
2014-05-01 20:35:36 +0000

    Initializer en un 60%, seguiré más tarde

--- trunk/Initializer/Initializer.sh
+++ trunk/Initializer/Initializer.sh
@@ -18,22 +18,39 @@ LOGEXT=""
 LOGSIZE=""
 
 # Variables que leemos del archivo de configuracion
-VARARCHCONF=("GRUPO" "CONFDIR" "BINDIR" "MAEDIR" "NOVEDIR" "DATASIZE" "ACEPDIR" "RECHDIR" "INFODIR" "LOGDIR" "LOGEXT" "LOGSIZE")
+VARARCHCONF=("CONFDIR" "MAEDIR" "NOVEDIR" "DATASIZE" "ACEPDIR" "RECHDIR" "INFODIR")
 
-# Variables que tienen información de path a directorios
-VARPATH=("GRUPO" "CONFDIR" "BINDIR" "MAEDIR" "NOVEDIR" "ACEPDIR" "RECHDIR" "INFODIR" "LOGDIR")
+# Variables para el log
+VARLOG=("BINDIR" "LOGDIR" "LOGEXT" "LOGSIZE")
 
-# Variales que tienen información de data (no path)
-VARLOG=("LOGEXT" "LOGSIZE")
+# Ejecutables
+VAREX=("logging.sh" "Start.sh" "Stop.sh" "Mover.sh")
 
-# Variable Datasize esta aparte
+# Mesajes generales
+REINSTALL="Por favor, vuelva a instalar el programa nuevamente.
+Para ello inicie el instalador y siga los pasos correctamente.
+Si el problema sigue persistiendo, intente reinciando el sistema."
 
 ### Paths base
 
 ACTUAL="`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd`"
 PATHGROUP=`echo "$ACTUAL" | sed "s-\(.*\)/grupo03/.*-\1/grupo03-"`
 PATHCONF="$PATHGROUP/conf"
+if ! [ -d $PATHCONF ]
+then
+	echo "No se encuentra directorio grupo03/conf.
+	No se puede inicializar RETAILC."
+	exit -2
+fi
 
+if ! [ -f "$PATHCONF/installer.conf" ]
+then
+	echo "No se encuentra el archivo de configuración.
+	No se puede inicializar RETAILC."
+	exit -2
+fi
+
+export GRUPO=`echo \`grep '^GRUPO' "$PATHCONF"/installer.conf\` | cut -f2 -d'='`
 ### Permisos de archivos
 
 # 755 rwx-rx-rx
@@ -42,21 +59,22 @@ PATHCONF="$PATHGROUP/conf"
 
 ### Funciones del script
 
-function iniciarLog {
-	# Inicio variable locales para comenzar el log, luego lo muevo a su carpeta correspondiente
-	# Y se setearan los valores correspondientes a las variables
-	export LOGSIZE=400
-	export LOGEXT="tmp"
-	export GRUPO=$PATHGROUP
-	export LOGDIR="$GRUPO"
-	
-	./logging.sh "Initializer" "Comando Initializer: Inicio de Ejecución"
-	
-	unset LOGDIR
-	unset LOGEXT
-	unset LOGSIZE
-	unset GRUPO
-
+function iniciarLog
+{
+	# Inicio variables para Log
+	for i in "${VARLOG[@]}"
+	do
+		export eval "$i"=$(echo `grep "^$i" $PATHCONF/installer.conf` | cut -f2 -d'=')
+		if [ -z "${!i}" ]
+		then
+			return 1
+		fi
+	done
+	if ! [ -f "$GRUPO/$BINDIR/logging.sh" ]
+	then
+		return 1
+	fi
+	$GRUPO/$BINDIR/logging.sh "Initializer" "Comando Initializer: Inicio de Ejecución"
 	return 0
 }
 
@@ -64,16 +82,45 @@ function cargarVariablesConf
 {
 	for i in "${VARARCHCONF[@]}"
 	do
-		eval "$i"=`echo \`grep '^"$i"' "$PATHCONF"/installer.conf\` | cut -f2 -d'='`
-		if [ -z "$i" ]
+		export eval "$i"=$(echo `grep "^$i" $PATHCONF/installer.conf` | cut -f2 -d'=')
+		if [ -z "${!i}" ]
 		then
-			return -1
+			return 1
 		fi
 	done
 
 	return 0
 }
 
+function verificarEx
+{
+	for x in "${VAREX[@]}"
+	do
+		if ! [ -f "$GRUPO/$BINDIR/$x" ]
+		then
+			return 1
+		fi
+	done
+
+	return 0
+}
+
+function verificarArchivos
+{
+	PRE="$GRUPO/$MAEDIR"
+	if ! [ -f $PRE/um.tab ]
+	then
+		return 1
+	elif ! [ -f $PRE/asociados.mae ]
+	then
+		return -1
+	elif ! [ -f $PRE/super.mae ]
+	then
+		return 1
+	fi
+
+	return 0
+}
 
 ### Comienzo script
 
@@ -87,5 +134,46 @@ then
 fi
 
 iniciarLog
+if [ $? -eq 1 ]
+then
+	echo "Faltan archivos o variables para el correcto funcionamiento del programa.
+$REINSTALL"
+exit -2
+fi
 
+cargarVariablesConf 
+if [ $? -eq 1 ]
+then
+	echo "Faltan variables en el archivo de configuración.
+$REINSTALL"
+$GRUPO/$BINDIR/logging.sh "Initializer" "Faltan variables en el archivo de configuración" "ERR"
+exit -2
+fi
+$GRUPO/$BINDIR/logging.sh "Initializer" "Se cargó satisfactoriamente el archivo de configuración" 
 
+verificarEx
+if [ $? -eq 1 ]
+then
+	echo "Faltan ejecutales para la correcta ejecución del programa.
+$REINSTALL"
+$GRUPO/$BINDIR/logging.sh "Initializer" "Faltan ejecutables en el directorio $BINDIR" "ERR"
+exit -2
+fi
+$GRUPO/$BINDIR/logging.sh "Initializer" "Se identificaron todos los ejecutables necesarios"
+
+verificarArchivos
+if [ $? -eq 1 ]
+then
+	echo "Faltan archivos maestros y/o tablas del sistema.
+$REINSTALL"
+$GRUPO/$BINDIR/logging.sh "Initializer" "Faltan archivos maestros y/o tablas del sistema" "ERR"
+exit -2
+fi
+$GRUPO/$BINDIR/logging.sh "Initializer" "Se encontraron los archivos maestros y tablas del sistema"
+
+if ! [ -z ENVINIT ]
+then
+	echo "El ambiente ya está inicializado, si quiere reiniciar termine su sesión e ingrese nuevamente"
+	$GRUPO/$BINDIR/logging.sh "Initializer" "Ambiente ya fue inicializado" "ERR"
+	exit -1
+fi
